f49419326645ba78b2ccd00f82b54d50b7430471
MaheyzaZahwaSaputra <maheyzasaputra20@gmail.com>
2024-02-19 02:24:00 +0700

    mengubah seusatu

--- hitungSegitiga.py
+++ hitungSegitiga.py
@@ -1,12 +1,12 @@
-alas = int(input("Masukan Luas: "))
-tinggi = int(input("Masukan Keliling: "))
+alas = int(input("Masukan Alas : "))
+tinggi = int(input("Masukan Tinggi : "))
 
-sisi1 = int(input(" Masukan Sisi 1: "))
-sisi2 = int(input(" Masukan Sisi 2: "))
-sisi3 = int(input(" Masukan Sisi 3: "))
+sisi1 = int(input(" Masukan Sisi 1 : "))
+sisi2 = int(input(" Masukan Sisi 2 : "))
+sisi3 = int(input(" Masukan Sisi 3 : "))
 
 luasSegitiga = 1/2 * alas * tinggi
 kelilingSegitiga = sisi1 + sisi2 + sisi3
 
-print("Luas Segitiga:  %.0f"%(luasSegitiga))
-print("Keliling Segitiga: %.0f"%(kelilingSegitiga))
+print("Luas Segitiga :  %.0f"%(luasSegitiga))
+print("Keliling Segitiga : %.0f"%(kelilingSegitiga))
--- hitungUmur.py
+++ hitungUmur.py
@@ -1,6 +1,6 @@
-tahunLahir = int(input("Masukkan tahun lahir: "))
-tahunSaatIni = int(input("Masukkan tahun saat ini: "))
+tahunLahir = int(input("Masukkan tahun lahir : "))
+tahunSaatIni = int(input("Masukkan tahun saat ini : "))
 
 umur = tahunSaatIni - tahunLahir
 
-print("Umur saat ini:", umur, "tahun")
+print("Umur saat ini : ", umur, "tahun")
